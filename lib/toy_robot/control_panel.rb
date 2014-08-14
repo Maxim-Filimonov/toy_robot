140b0e8416fa65f42b9e8bed196d02df52409a58
Maxim Filimonov <tpaktopsp@gmail.com>
2014-08-14 17:34:33 +1000

    Plug report command to display

--- lib/toy_robot/control_panel.rb
+++ lib/toy_robot/control_panel.rb
@@ -6,26 +6,36 @@ require 'toy_robot/commands/null_command'
 module ToyRobot
   class ControlPanel
     attr_accessor :robot
-    attr_reader :init_blueprints, :action_blueprints
+    attr_reader :init_blueprints, :action_blueprints, :display
 
-    def initialize(init_blueprints: [ToyRobot::Commands::ParseCommand],
-      action_blueprints: [ToyRobot::Commands::MoveCommand])
+    def initialize(init_blueprints: default_init_blueprints,
+      action_blueprints: default_action_blueprints)
       @init_blueprints = init_blueprints
       @action_blueprints = action_blueprints
+    end
+
+    def default_init_blueprints
+      [ToyRobot::Commands::ParseCommand]
+    end
+
+    def default_action_blueprints
+        [
+          ToyRobot::Commands::MoveCommand,
+          ToyRobot::Commands::ReportCommand
+        ]
     end
 
     def run(raw_command)
       if robot
         command = match_command(action_blueprints, raw_command)
-        command.execute(robot)
+        result = command.execute(robot)
+        if result
+          @display = result
+        end
       else
         command = match_command(init_blueprints, raw_command)
         @robot = command.execute
       end
-    end
-
-    def display
-      '0,1,NORTH'
     end
 
     private
--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -8,6 +8,15 @@ module ToyRobot
     end
 
     def report
+      {
+        nav: {
+          x: 0,
+          y: 1
+        },
+        compass: {
+          direction: "NORTH"
+        }
+      }
     end
 
     private

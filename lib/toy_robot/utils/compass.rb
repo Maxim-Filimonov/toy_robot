43831330af7adba4dd275adf12c65ed6439f2ba6
Maxim Filimonov <tpaktopsp@gmail.com>
2014-08-15 09:53:14 +1000

    Make compass less magical

--- lib/toy_robot/utils/compass.rb
+++ lib/toy_robot/utils/compass.rb
@@ -3,51 +3,61 @@ module ToyRobot
     class Compass
       CARDINAL_DIRECTIONS = [:north, :east, :south, :west]
 
-      def self.cardinal_directions
-        CARDINAL_DIRECTIONS
-      end
+      class << self
+        def cardinal_directions
+          CARDINAL_DIRECTIONS
+        end
 
-      CARDINAL_DIRECTIONS.each do |direction|
-        self.class.class_eval do
-          define_method direction do
-            direction
+        def north
+          CARDINAL_DIRECTIONS[0]
+        end
+
+        def east
+          CARDINAL_DIRECTIONS[1]
+        end
+
+        def south
+          CARDINAL_DIRECTIONS[2]
+        end
+
+        def west
+          CARDINAL_DIRECTIONS[3]
+        end
+
+        def opposite(direction)
+          index = CARDINAL_DIRECTIONS.index(direction)
+          index += 2
+          direction_by_index(index)
+        end
+
+        def left_from(direction)
+          index = CARDINAL_DIRECTIONS.index(direction)
+          index -= 1
+          direction_by_index(index)
+        end
+
+        def right_from(direction)
+          index = CARDINAL_DIRECTIONS.index(direction)
+          index += 1
+          direction_by_index(index)
+        end
+
+        def to_direction(string_direction)
+          self.send(string_direction.downcase.to_sym)
+        end
+
+        private
+        def direction_by_index(index)
+          if index >= CARDINAL_DIRECTIONS.length
+            CARDINAL_DIRECTIONS[index - CARDINAL_DIRECTIONS.length]
+          elsif index < 0
+            CARDINAL_DIRECTIONS[CARDINAL_DIRECTIONS.length + index]
+          else
+            CARDINAL_DIRECTIONS[index]
           end
         end
       end
 
-      def self.opposite(direction)
-        index = CARDINAL_DIRECTIONS.index(direction)
-        index += 2
-        direction_by_index(index)
-      end
-
-      def self.left_from(direction)
-        index = CARDINAL_DIRECTIONS.index(direction)
-        index -= 1
-        direction_by_index(index)
-      end
-
-      def self.right_from(direction)
-        index = CARDINAL_DIRECTIONS.index(direction)
-        index += 1
-        direction_by_index(index)
-      end
-
-      def self.to_direction(string_direction)
-        self.send(string_direction.downcase.to_sym)
-      end
-
-      private
-      def self.direction_by_index(index)
-        if index >= CARDINAL_DIRECTIONS.length
-          CARDINAL_DIRECTIONS[index - CARDINAL_DIRECTIONS.length]
-        elsif index < 0
-          CARDINAL_DIRECTIONS[CARDINAL_DIRECTIONS.length + index]
-        else
-          CARDINAL_DIRECTIONS[index]
-        end
-      end
     end
-    
   end
 end
